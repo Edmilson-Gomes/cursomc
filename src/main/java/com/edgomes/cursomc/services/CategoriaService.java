7eb0a8e5c55864835a1e1846987711d3b5662e19
Edmilson Gomes <edmilsongomes@live.com>
2021-08-11 10:46:25 -0300

    Criando repository e service para cotegoria

---
+++ src/main/java/com/edgomes/cursomc/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.edgomes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edgomes.cursomc.domain.Categoria;
+import com.edgomes.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repo;
+	public Categoria find(Integer id){
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		}
+}
